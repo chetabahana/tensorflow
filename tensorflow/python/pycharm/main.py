3209a30473b4e899405ce36034e82a3ac5380246
Chetabahana <chetabahana@gmail.com>
2021-06-09 08:37:44 +0700

    Create PyCharm Project

---
+++ tensorflow/python/pycharm/main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.compat.v1.Session()
+print(sess.run(hello))
