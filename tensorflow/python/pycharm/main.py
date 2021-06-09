04eadd3a4570cad6a5896aafabc4ef8b781e2070
Chetabahana <chetabahana@gmail.com>
2021-06-10 00:18:14 +0700

    Update main.py

--- tensorflow/python/pycharm/main.py
+++ tensorflow/python/pycharm/main.py
@@ -3,13 +3,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import tensorflow as tf
-tf.compat.v1.disable_eager_execution()
-hello = tf.constant('Hello, TensorFlow!')
-sess = tf.compat.v1.Session()
-print(sess.run(hello))
-
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 # y = Wx + b
 
