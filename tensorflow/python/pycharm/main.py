fd5ea5a72db846792e37d9aaac0c80c8a9e8f7c3
Chetabahana <chetabahana@gmail.com>
2021-06-20 18:03:38 +0700

    Update main.py

--- tensorflow/python/pycharm/main.py
+++ tensorflow/python/pycharm/main.py
@@ -13,48 +13,48 @@ print("System path: ", os.environ.get('PATH'))
 #https://www.tensorflow.org/guide/migrate
 
 import tensorflow as tf
-tf.compat.v1.disable_v2_behavior()
+#import tensorflow.compat.v1 as tf
+#tf.disable_v2_behavior()
 
 print("\nNum GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-print("Tensorflow version: ", tf.__version__)
+print("Tensorflow version: ", tf.__version__, "\n")
 
 # Formula
 # y = Wx + b
 
 # Training data, given x_train as inputs, we expect y_train as outputs
-x_train = [1.0, 2.0, 3.0, 4.0]
-y_train = [-1.0, -2.0, -3.0, -4.0]
+#x_train = [1.0, 2.0, 3.0, 4.0]
+#y_train = [-1.0, -2.0, -3.0, -4.0]
+
+x_train = tf.constant([1.0, 2.0, 3.0, 4.0])
+y_train = tf.constant([-1.0, -2.0, -3.0, -4.0])
+
+print("x_train = ", x_train)
+print("y_train = ", y_train, "\n")
 
 # Graph construction
 # W and b are variables that our model will change
-W = tf.Variable(initial_value=[1.0], dtype=tf.float32)
-b = tf.Variable(initial_value=[1.0], dtype=tf.float32)
+#W = tf.Variable(initial_value=[1.0], dtype=tf.float32)
+#b = tf.Variable(initial_value=[1.0], dtype=tf.float32)
+W = tf.Variable(tf.zeros(shape=(1,)), name="W")
+b = tf.Variable(tf.ones(shape=(1,)), name="b")
 
-# x is an input placeholder and y is a placeholder used to tell model what correct answers are
-x = tf.compat.v1.placeholder(dtype=tf.float32)
-y_input = tf.compat.v1.placeholder(dtype=tf.float32)
+print("W = ", W)
+print("b = ", b, "\n")
+
+#x is an input placeholder and y is a placeholder used to tell model what correct answers are
+#x = tf.placeholder(dtype=tf.float32)
+#print("x = ", x)
+#y_input = tf.placeholder(dtype=tf.float32)
+#print("y_input = ", y_input, "\n")
 
 # y_output is the formula we are trying to follow to produce an output given input from x
-y_output = W * x + b
+#y_output = W * x + b
 
-# Loss function and optimizer aim to minimize the difference between actual and expected outputs (total sums)
-loss = tf.reduce_sum(input_tensor=tf.square(x=y_output - y_input))
-optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.01)
-train_step = optimizer.minimize(loss=loss)
+@tf.function
+def forward(x):
+  return W * x + b
 
-# Sessions are used to evaluate the tensor value of a node or nodes
-session = tf.compat.v1.Session()
-session.run(tf.compat.v1.global_variables_initializer())
+y_output = forward(x_train)
+print("y_output = ", y_output)
 
-# Total loss before training
-print("\nTotal kerugian sebelum training:", session.run(fetches=loss, feed_dict={x: x_train, y_input: y_train}))
-
-# Training phase, run the train step 1000 times
-for _ in range(1000):
-    session.run(fetches=train_step, feed_dict={x: x_train, y_input: y_train})
-
-# Total loss and modified W and b values after training
-print("Total kerugian setelah training:", session.run(fetches=[loss, W, b], feed_dict={x: x_train, y_input: y_train}))
-
-# Test the model with some new values
-print("Test kerugian dengan model baru:", session.run(fetches=y_output, feed_dict={x: [5.0, 10.0, 15.0]}))
