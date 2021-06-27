74b47964da6850a7397b04013f68854c6c691334
Chetabahana <chetabahana@gmail.com>
2021-06-27 10:20:20 +0700

    Update main.py

--- tensorflow/python/pycharm/main.py
+++ tensorflow/python/pycharm/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 '''  Test The Model Accuracy on Existing Data  '''
 
 import numpy as np
@@ -47,6 +48,57 @@ model.add(Dense(units=1)) #Prediction of the next closing value
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
+=======
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+
+#Load Data
+company = 'FB'
+
+start = dt.datetime(2012,1,1)
+end = dt.datetime(2020,1,1)
+
+data = web.DataReader(company, 'yahoo', start, end)
+
+#Prepare Data
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x,0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+#Build The Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1)) #Prediction of the next closing value
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
+
+'''  Test The Model Accuracy on Existing Data  '''
+>>>>>>> Stashed changes
 
 #Load Test Data
 test_start = dt.datetime(2012,1,1)
