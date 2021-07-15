7114a592ec010881bfb4c962f306d86711f14498
Chetabahana <chetabahana@gmail.com>
2021-07-15 14:50:49 +0700

    Update main.py

--- tensorflow/python/pycharm/main.py
+++ tensorflow/python/pycharm/main.py
@@ -1,2 +1,12 @@
 # Description: This program uses an artificial neural network called Long Short Term Memory (LSTM)
 # to predict the closing stock price of a corporation (Apple Inc.) using the past 60 days stock price.
+
+import math
+import pandas_datareader as web
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
